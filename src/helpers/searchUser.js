d8637009341d750b2a28157366fe270c832baf67
Ayodele Segun Flexible <52238200+segun-flexible@users.noreply.github.com>
2020-09-27 20:00:43 +0100

    Update searchUser.js

--- src/helpers/searchUser.js
+++ src/helpers/searchUser.js
@@ -1,7 +1,7 @@
 import trimText from "./trimText";
 
 const searchUser = (str) => {
-alert(`https://api.github.com/users/${trimText(str)}`.trim()) 
+alert(trimText(str)) 
   const result = fetch(`https://api.github.com/users/${trimText(str)}`)
     .then((res) => (res.status < 299 ? res.json() : res.statusText))
     .catch((err) => err.message);
